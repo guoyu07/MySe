98ac40b0604344c23f373a89462952e7083f2031
gsh199449 <gsh199449@hotmail.com>
2013-11-29 23:43:32 +0800

    添加了一些包和TestCase

---
+++ test/com/gs/test/TestAction.java
@@ -0,0 +1,14 @@
+package com.gs.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAction {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
