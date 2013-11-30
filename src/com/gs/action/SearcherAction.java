7e17168b38b164c3975fa7e5c4d08e21bc1bfdb0
gsh199449 <gsh199449@hotmail.com>
2013-11-30 16:17:04 +0800

    添加配置文件和Action

---
+++ src/com/gs/action/SearcherAction.java
@@ -0,0 +1,10 @@
+package com.gs.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SearcherAction extends ActionSupport {
+
+	private static final long serialVersionUID = -5206663242253958019L; 
+	
+	
+}
