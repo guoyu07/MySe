d9932bce9e621654d5166ed96e453fce761fef76
gsh199449 <gsh199449@hotmail.com>
2013-11-30 22:26:27 +0800

    加入action逻辑

--- src/com/gs/action/SearcherAction.java
+++ src/com/gs/action/SearcherAction.java
@@ -1,10 +1,32 @@
 package com.gs.action;
 
+import java.util.LinkedList;
+
+import com.gs.searcher.Hit;
+import com.gs.searcher.Searcher;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class SearcherAction extends ActionSupport {
 
 	private static final long serialVersionUID = -5206663242253958019L; 
-	
+	private LinkedList<Hit> list;
+	public String search(){
+		Searcher s = new Searcher("D://Test//json");
+		list = s.search("中国");
+		System.out.println(list);
+		return SUCCESS;
+	}
+	/**
+	 * @return the list
+	 */
+	public LinkedList<Hit> getList() {
+		return list;
+	}
+	/**
+	 * @param list the list to set
+	 */
+	public void setList(LinkedList<Hit> list) {
+		this.list = list;
+	}
 	
 }
